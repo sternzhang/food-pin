7dcfb34233879b5fb3e3101e4bfff5122ef122e3
zhangshiqiang.stern <zhangshiqiang.stern@bytedance.com>
2022-10-29 08:10:57 +0800

    feat: 课程9完成

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by stern zhang on 2022/10/28.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = 20
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -13,18 +13,32 @@ enum Section {
 
 class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "P etite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" , "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Av enue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Ko ng", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "N ew York", "London", "London", "London", "London"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "Ame rican / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", " British", "Thai"]
+    
+    lazy var dataSource = configureDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        tableView.separatorStyle = .none
+        tableView.dataSource = dataSource
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(restaurantNames, toSection: .all)
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
     
     func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
         let cellIdentifier = "datacell"
         let dataSource = UITableViewDiffableDataSource<Section, String>(
-            tableView: tableView, cellProvider: {tableView, IndexPath, restaurantName in
-                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: IndexPath)
-                cell.textLabel?.text = restaurantName
-                cell.imageView?.image = UIImage(named: "restaurant")
+            tableView: tableView, cellProvider: {tableView, indexPath, restaurantName in
+                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+                cell.nameLabel.text = restaurantName
+                cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+                cell.typeLabel.text = self.restaurantTypes[indexPath.row]
+                cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
                 
                 return cell
             }
