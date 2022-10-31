ae651a73957a1127cd839254f2b49a0a94c58302
张世强-晖致-前端 <shiqiang.zhang@viatris.com>
2022-10-31 17:13:33 +0800

    feat: 完整Restaurant数据结构

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
     @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
     
-    var restaurantImageName = ""
+    var restaurant = Restaurant()
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
-        restaurantImageView.image = UIImage(named: restaurantImageName)
+        restaurantImageView.image = UIImage(named: restaurant.image)
+        nameLabel.text = restaurant.name
+        typeLabel.text = restaurant.type
+        locationLabel.text = restaurant.location
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
