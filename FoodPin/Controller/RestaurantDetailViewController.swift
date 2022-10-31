9f70a927f2bf8ed8d76609e2f101e7ccfd811b70
zhangshiqiang.stern <zhangshiqiang.stern@bytedance.com>
2022-10-31 21:56:28 +0800

    feat: 自定义detailview

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -15,7 +15,9 @@ class RestaurantDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = false
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
         //configure header view
         headerView.nameLabel.text = restaurant.name
         headerView.typeLabel.text = restaurant.type
@@ -36,4 +38,29 @@ class RestaurantDetailViewController: UIViewController {
     }
     */
 
+}
+
+extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
+            cell.column1TitleLabel.text = "Address"
+            cell.column1TextLabel.text = restaurant.location
+            cell.column2TitleLabel.text = "Phone"
+            cell.column2TextLabel.text = restaurant.phone
+            return cell
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
+    
 }
